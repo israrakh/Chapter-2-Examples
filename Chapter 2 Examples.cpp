c0bc7ed15fea3b427db8c3506f0052fa4d7fd39c
Israr <israr.akh03@gmail.com>
2024-09-08 10:47:21 -0400

    source code has been modified for Cal-a-Disc

--- Chapter 2 Examples.cpp
+++ Chapter 2 Examples.cpp
@@ -4,23 +4,6 @@
 using namespace std;
 int main()
 {
-// This program calculates and displays the several values
-// and various percentages for those values.
-	double salary = 6000;
-	double percent1 = 5, percent2 = 7, percent3 = 10;
-	double contribution1, contribution2, contribution3;
-	contribution1 = salary * percent1 / 100;
-	contribution2 = salary * percent2 / 100;
-	contribution3 = salary * percent3 / 100;
-	cout << "Amount of contribution at " << percent1 << "% is $" <<
-		contribution1 << endl;
-	cout << "Amount of contribution at " << percent2 << "% is $" <<
-		contribution2 << endl;
-	cout << "Amount of contribution at " << percent3 << "% is $" <<
-		contribution3 << endl;
-	
-	
-	cout << "_______________________________________________" << endl;
 
 //This program calculates and displays the discounted value of an item.
 
